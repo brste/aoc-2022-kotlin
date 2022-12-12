aae135d034979740fefc64be4716d637006cd015
Stefan Brand <stefan.brand@landhaus-shop.de>
2022-12-13 00:09:42 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun topK(input: List<String>, k: Int) : List<Int> {
+        val caloriesOfElves = mutableListOf<Int>()
+        var sum = 0
+        for (line in input) {
+            if (line.isBlank()) {
+                caloriesOfElves.add(sum)
+                sum = 0
+            } else {
+                sum += line.toInt()
+            }
+        }
+        // empty line at the end -> we don't need to do a last caloriesOfElves.ad(sum)
+
+        return caloriesOfElves.sortedDescending().take(k)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = topK(input, 1).first()
+
+    fun part2(input: List<String>): Int = topK(input, 3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
